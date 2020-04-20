b1d4e816417734e4fc0bc908c7496ba682deabe3
virajmavani <viraj.mavani1996@gmail.com>
2020-04-19 20:36:24 -0500

    Refactoring

--- longProject4/src/rdb170002/MDS.java
+++ longProject4/src/rdb170002/MDS.java
@@ -9,10 +9,10 @@ import java.util.*;
 public class MDS {
 
 	//Map with id as key and entry as value
-	private Map<Long, Entry> entryMap;
+	private Map<Long, Entry> entry_hmap;
 	
 	//Map with map of map with description as key and map of price and price's counter as value
-    private Map<Long, Map<Money, Integer>> setMap;
+    private Map<Long, Map<Money, Integer>> set_hmap;
 
 
     private static class Entry implements Comparable {
@@ -60,8 +60,8 @@ public class MDS {
 	 */
 
     public MDS() {
-        entryMap = new HashMap<>();
-        setMap = new HashMap<>();
+        entry_hmap = new HashMap<>();
+        set_hmap = new HashMap<>();
     }
 
 	/**
@@ -78,14 +78,18 @@ public class MDS {
 	 */
 
     public int insert(long id, Money price, java.util.List<Long> list) {
-        int result = entryMap.get(id) == null ? 1 : 0;
+        int result = 0;
+        if ( entry_hmap.get(id) == null ) {
+            result = 1;
+        }
 
-        Entry e = result == 0 ? entryMap.get(id) : new Entry(id, price, new HashSet<>(list));
+        Entry e = result == 0 ? entry_hmap.get(id) : new Entry(id, price, new HashSet<>(list));
         if (result == 0) {
             for (long x : e.description) {
-                if (setMap.get(x).get(e.price) != null) {
-                    setMap.get(x).put(e.price, setMap.get(x).get(e.price) - 1);
-                    if (setMap.get(x).get(e.price) <= 0) setMap.get(x).remove(e.price);
+                Map<Money, Integer> m = set_hmap.get(x);
+                if (m.get(e.price) != null) {
+                    m.put(e.price, m.get(e.price) - 1);
+                    if (m.get(e.price) <= 0) m.remove(e.price);
                 }
             }
             if (list != null && list.size() > 0) {
@@ -96,16 +100,18 @@ public class MDS {
             e.price = price;
         }
         for (Long x : e.description) {
-            if (setMap.get(x) == null) {
-                setMap.put(x, new TreeMap<>());
+            Map<Money, Integer> m = set_hmap.get(x);
+            if (m == null) {
+                set_hmap.put(x, new TreeMap<>());
+                m = set_hmap.get(x);
             }
-            if (setMap.get(x).get(e.price) == null) {
-                setMap.get(x).put(e.price, 1);
+            if (m.get(e.price) == null) {
+                m.put(e.price, 1);
             } else {
-                setMap.get(x).put(e.price, setMap.get(x).get(e.price) + 1);
+                m.put(e.price, m.get(e.price) + 1);
             }
         }
-        entryMap.put(id, e);
+        entry_hmap.put(id, e);
 
         return result;
     }
@@ -117,7 +123,11 @@ public class MDS {
 	 */
 	
     public Money find(long id) {
-        Money money = entryMap.get(id) != null ? entryMap.get(id).price : new Money();
+        Money money = new Money();
+        if ( entry_hmap.get(id) != null ) {
+            money = entry_hmap.get(id).price;
+        }
+
         return money;
     }
 
@@ -131,13 +141,14 @@ public class MDS {
 
     public long delete(long id) {
         long sum = 0;
-        if (entryMap.get(id) != null) {
-            Entry e = entryMap.remove(id);
+        if ( entry_hmap.get(id) != null ) {
+            Entry e = entry_hmap.remove(id);
             for (long x : e.description) {
                 sum += x;
-                if (setMap.get(x).get(e.price) != null) {
-                    setMap.get(x).put(e.price, setMap.get(x).get(e.price) - 1);
-                    if (setMap.get(x).get(e.price) <= 0) setMap.get(x).remove(e.price);
+                Map<Money, Integer> m = set_hmap.get(x);
+                if (m.get(e.price) != null) {
+                    m.put(e.price, m.get(e.price) - 1);
+                    if (m.get(e.price) <= 0) m.remove(e.price);
                 }
             }
         }
@@ -157,7 +168,7 @@ public class MDS {
 
     public Money findMinPrice(long n) {
         Money money = new Money();
-        TreeMap<Money, Integer> map = (TreeMap<Money, Integer>) setMap.get(n);
+        TreeMap<Money, Integer> map = (TreeMap<Money, Integer>) set_hmap.get(n);
         if (map != null && map.size() > 0) {
             money = map.firstKey();
         }
@@ -175,7 +186,7 @@ public class MDS {
 
     public Money findMaxPrice(long n) {
         Money money = new Money();
-        TreeMap<Money, Integer> map = (TreeMap<Money, Integer>) setMap.get(n);
+        TreeMap<Money, Integer> map = (TreeMap<Money, Integer>) set_hmap.get(n);
         if (map != null && map.size() > 0) {
             money = map.lastKey();
 
@@ -193,7 +204,7 @@ public class MDS {
 	 * @return
 	 */
     public int findPriceRange(long n, Money low, Money high) {
-        TreeMap<Money, Integer> map = (TreeMap<Money, Integer>) setMap.get(n);
+        TreeMap<Money, Integer> map = (TreeMap<Money, Integer>) set_hmap.get(n);
         int i = 0;
         for (Money e : map.keySet()) {
             if (e.compareTo(low) >= 0 && e.compareTo(high) <= 0) {
@@ -217,15 +228,15 @@ public class MDS {
     public Money priceHike(long l, long h, double rate) {
         long hike = 0;
         while (l <= h) {
-            if (entryMap.get(l) != null) {
-                Money oldPrice = entryMap.get(l).price;
-                long op = (oldPrice.dollars() * 100) + oldPrice.cents();
-                long np = Double.valueOf(op * (1 + (0.01 * rate))).longValue();
-                long dollars = np / 100;
-                int cents = Long.valueOf(np % 100).intValue();
-                Money newPrice = new Money(dollars, cents);
-                insert(entryMap.get(l).id, newPrice, new ArrayList<>());
-                hike += (np - op);
+            if (entry_hmap.get(l) != null) {
+                Money past_price = entry_hmap.get(l).price;
+                long past_p = (past_price.dollars() * 100) + past_price.cents();
+                long new_p = Double.valueOf(past_p * (1 + (0.01 * rate))).longValue();
+                long dollars = new_p / 100;
+                int cents = Long.valueOf(new_p % 100).intValue();
+                Money new_price = new Money(dollars, cents);
+                insert(entry_hmap.get(l).id, new_price, new ArrayList<>());
+                hike += (new_p - past_p);
             }
 
             l++;
@@ -245,14 +256,14 @@ public class MDS {
 	 */
 
     public long removeNames(long id, java.util.List<Long> list) {
-        Entry e = entryMap.get(id);
+        Entry e = entry_hmap.get(id);
         long sum = 0;
         for (Long x : list) {
             if (e.description.remove(x)) {
                 sum += x;
-                setMap.get(x).put(e.price, setMap.get(x).get(e.price) - 1);
-                if (setMap.get(x).get(e.price) <= 0) setMap.get(x).remove(e.price);
-                if (setMap.get(x).keySet().size() == 0) setMap.remove(x);
+                set_hmap.get(x).put(e.price, set_hmap.get(x).get(e.price) - 1);
+                if (set_hmap.get(x).get(e.price) <= 0) set_hmap.get(x).remove(e.price);
+                if (set_hmap.get(x).keySet().size() == 0) set_hmap.remove(x);
             }
         }
         return sum;
